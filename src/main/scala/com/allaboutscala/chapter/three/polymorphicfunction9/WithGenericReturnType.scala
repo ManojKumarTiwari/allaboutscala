ae5acad37d37239b4d7400859196641483b19fba
Manoj Kumar Tiwari <manojtiwari1.1v@gmail.com>
2019-02-05 15:58:48 +0530

    Polymorphic function with generic return type

---
+++ src/main/scala/com/allaboutscala/chapter/three/polymorphicfunction9/WithGenericReturnType.scala
@@ -0,0 +1,31 @@
+package com.allaboutscala.chapter.three.polymorphicfunction9
+
+object WithGenericReturnType extends App {
+
+  println("\nStep 3: How to define a generic typed function which also has a generic return type")
+  def applyDiscountWithReturnType[T](discount: T): Seq[T] = {
+    discount match {
+      case d: String =>
+        println(s"Lookup percentage discount in database for $d")
+        Seq[T](discount)
+
+      case d: Double =>
+        println(s"$d discount will be applied")
+        Seq[T](discount)
+
+      case d @ _ =>
+        println("Unsupported discount type")
+        Seq[T](discount)
+    }
+  }
+
+  println("\nStep 4: How to call a generic typed function which also has a generic return type")
+  println(s"Result of applyDiscountWithReturnType with String parameter = ${applyDiscountWithReturnType[String]("COUPON_123")}")
+
+  println()
+  println(s"Result of applyDiscountWithReturnType with Double parameter = ${applyDiscountWithReturnType[Double](10.5)}")
+
+  println()
+  println(s"Result of applyDiscountWithReturnType with Char parameter = ${applyDiscountWithReturnType[Char]('U')}")
+
+}
